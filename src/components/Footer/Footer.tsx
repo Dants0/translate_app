3d54758af86af1941bc71c8c5aa0fc783b000df9
Guilherme Dantas Goes <guilhermedg@labcmi.org.br>
2024-02-06 16:45:39 -0300

    add: donate button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,32 @@
-import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
-import styles from './styles.module.scss'
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import styles from "./styles.module.scss";
 
 export const Footer = () => {
   return (
     <footer className={styles.f}>
-        Translate App
-
-        <div className={styles.logos}>
-        <a href="https://github.com/Dants0/" target='_blank'><GitHubLogoIcon width={35} height={100} color="white"/></a>
-        <a href="https://www.linkedin.com/in/guilherme-góes-8b72531b0/" target='_blank'><LinkedInLogoIcon  color="white" width={35} height={100}/></a>
-        </div>
+      Translate App
+      <div className={styles.logos}>
+        <a href="https://github.com/Dants0/" target="_blank">
+          <GitHubLogoIcon width={35} height={100} color="white" />
+        </a>
+        <a href="https://ko-fi.com/nosensee" target="_blank" rel="noreferrer">
+          <img
+            height="36"
+            style={{
+              border: "none",
+              width: "100%",
+            }}
+            src="https://storage.ko-fi.com/cdn/kofi2.png?v=3"
+            alt="Buy Me a Coffee at ko-fi.com"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/guilherme-góes-8b72531b0/"
+          target="_blank"
+        >
+          <LinkedInLogoIcon color="white" width={35} height={100} />
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
