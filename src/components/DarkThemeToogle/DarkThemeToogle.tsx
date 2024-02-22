ce0be4016b9702f7e415eeca3a2301894f8ab867
Guilherme Dantas Goes <guilhermedg@labcmi.org.br>
2024-02-22 08:08:01 -0300

    remove: two icons dark theme and light theme

--- src/components/DarkThemeToogle/DarkThemeToogle.tsx
+++ src/components/DarkThemeToogle/DarkThemeToogle.tsx
@@ -6,11 +6,13 @@ import styles from './styles.module.scss'
 export const DarkThemeToggle = () => {
   const { isDark, setIsDark } = useColorScheme();
 
+  console.log(isDark)
+
   return (
     <Toggle
       checked={isDark}
       onChange={({ target }) => setIsDark(target.checked)}
-      icons={{ checked: <span className={styles.icons}>🌙</span>, unchecked: <span  className={styles.icons}>🔆</span> }}
+      icons={{ checked: <span className={styles.icons}>{isDark ? <>🌙</> : <>☀️</>}</span>, unchecked: <span  className={styles.icons}></span> }}
       aria-label="Dark mode toggle"
       hidden
     />
